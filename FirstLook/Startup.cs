b6509cf4beba6ea65e6f5b305953e4151f467b22
tothp <tothp@ASUS5>
2017-03-15 23:00:01 +0100

    Add project files.

---
+++ FirstLook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstLook.Startup))]
+namespace FirstLook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
