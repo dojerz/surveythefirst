d5c02890ae0123f66660f75c660cbc71da58abbd
Pa Da <Padama12@freemail.hu>
2017-03-26 22:58:33 +0200

    Műveletek

--- FirstLook/Controllers/BaseController.cs
+++ FirstLook/Controllers/BaseController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Data.Entity;
 
 namespace FirstLook.Controllers
 {
@@ -26,10 +28,13 @@ namespace FirstLook.Controllers
         // POST: Base/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,HelyszinMegnev,Megnevezes,EpitmenytipusID,WGsLAT,WgsLON")] Bazis b)
+        public ActionResult Create([Bind(Include = "HelyszinMegnev,Megnevezes,EpitmenytipusID,WGsLAT,WgsLON")] Bazis b)
         {
             if (ModelState.IsValid)
             {
+                var existingBases = bazisok.Bazis.ToList();
+                var maxId = existingBases.Max(i => i.Id);
+                b.Id = maxId + 1;
                 /*Bazis b = new Bazis();
                 b.Id = 3;
                 b.HelyszinMegnev = Place;
@@ -43,6 +48,85 @@ namespace FirstLook.Controllers
             }
 
             return View();
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bazis b = bazisok.Bazis.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        // POST: Movies/Edit/5 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,HelyszinMegnev,Megnevezes,EpitmenytipusID,WGsLAT,WgsLON")] Bazis b)
+        {
+            if (ModelState.IsValid)
+            {
+                bazisok.Entry(b).State = EntityState.Modified;
+                bazisok.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(b);
+        }
+
+        // GET: Movies/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bazis b = bazisok.Bazis.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        // GET: Movies/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bazis b = bazisok.Bazis.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bazis b = bazisok.Bazis.Find(id);
+            bazisok.Bazis.Remove(b);
+            bazisok.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                bazisok.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
